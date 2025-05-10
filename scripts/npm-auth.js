42cdd8596dede68f4969391e8fb1571706568675
KevinComba <kelchospense88@gmail.com>
2025-05-10 19:34:11 +0300

    feat: enhance publishing workflow to support both GitHub Packages and npm registry

---
+++ scripts/npm-auth.js
@@ -0,0 +1,9 @@
+﻿// Login to npm
+console.log('Please run the following commands to authenticate with npm:');
+console.log('');
+console.log('1. Create a token on npm website (https://www.npmjs.com/settings/YOUR_USERNAME/tokens)');
+console.log('2. Log in with your credentials:');
+console.log('   npm login --registry=https://registry.npmjs.org/');
+console.log('');
+console.log('For GitHub Packages authentication:');
+console.log('   npm login --registry=https://npm.pkg.github.com/ --scope=@kelcho-spense');
